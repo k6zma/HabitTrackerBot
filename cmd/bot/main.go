7d4c3491e73fab8b88addaa0dd60cf45cf34f0d9
k6zma <87338187+k6zma@users.noreply.github.com>
2024-07-09 01:38:26 +0300

    feat: added starting logic of bot

---
+++ cmd/bot/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/k6zma/HabitTrackerBot/internal/bot"
+
+func main() {
+	bot.Start()
+}
---
+++ internal/bot/bot.go
@@ -0,0 +1,43 @@
+package bot
+
+import (
+	"fmt"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"github.com/k6zma/HabitTrackerBot/internal/utils"
+)
+
+func Start() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		utils.PanicWithColor(fmt.Sprintf("Error occrured while loading .env file: %v", err))
+	}
+
+	utils.SuccessfulExecution("reading .env file")
+
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		utils.PanicWithColor(err)
+	}
+
+	utils.SuccessfulExecution("connecting a bot using a token")
+
+	bot.Debug = true
+
+	utils.SuccessfulAuthorizing(bot)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			// TODO: add message handling
+		}
+	}
+}
