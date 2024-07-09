bbe59da4ca09349ce685c1a15d70b741b49e63fb
k6zma <87338187+k6zma@users.noreply.github.com>
2024-07-09 11:20:49 +0300

    fixed: message in start command

--- internal/bot/commands.go
+++ internal/bot/commands.go
@@ -4,7 +4,8 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func startCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Привет! Я бот для отслеживания привычек. "+
-		"Я помогу вам добавить новые привычки, отмечать их выполнение и следить за прогрессом.",
+		"Я помогу вам добавить новые привычки, отмечать их выполнение и следить за прогрессом.\n"+
+		"Воспользуйтесь командой /help, чтобы посмотреть все доступные команды.",
 	)
 	msg.ReplyToMessageID = inputMessage.MessageID
 
