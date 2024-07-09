85bcc5661c7e341a67290fda16648f929173eca6
k6zma <87338187+k6zma@users.noreply.github.com>
2024-07-09 11:22:34 +0300

    fixed: paragraph style in start command

--- internal/bot/commands.go
+++ internal/bot/commands.go
@@ -4,7 +4,7 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func startCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Привет! Я бот для отслеживания привычек. "+
-		"Я помогу вам добавить новые привычки, отмечать их выполнение и следить за прогрессом.\n"+
+		"Я помогу вам добавить новые привычки, отмечать их выполнение и следить за прогрессом.\n\n"+
 		"Воспользуйтесь командой /help, чтобы посмотреть все доступные команды.",
 	)
 	msg.ReplyToMessageID = inputMessage.MessageID
