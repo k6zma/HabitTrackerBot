f7dfcf8a2c563da009b8e908e26ccc901d6ef06e
k6zma <87338187+k6zma@users.noreply.github.com>
2024-07-09 11:13:40 +0300

    fixed: style of start command

--- internal/bot/commands.go
+++ internal/bot/commands.go
@@ -4,7 +4,8 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func startCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Привет! Я бот для отслеживания привычек. "+
-		"Я помогу вам добавить новые привычки, отмечать их выполнение и следить за прогрессом.")
+		"Я помогу вам добавить новые привычки, отмечать их выполнение и следить за прогрессом.",
+	)
 	msg.ReplyToMessageID = inputMessage.MessageID
 
 	bot.Send(msg)
